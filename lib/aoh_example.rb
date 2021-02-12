967fb74f66d848dfb117c3f2fa8eedf7e2b0cd8a
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-12 19:57:06 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,12 +8,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   
- a_single_array =  [
-   {:name  =>"Don Gately", :occupation =>"Live-in Staff" },
-   {:name  =>"Joelle van Dyne", :occupation =>"Radio Personality"},
-   {:name  =>"Pat Monteseian", :occupation =>"Staff" },
-   {:name  =>"Kate Gompert", :occupation =>"None" },
-   {:name  =>"Bruce Green", :occupation =>"Fan of Mildred"}]
+[{:or => "nest"}]
   
    
   # Build an array that contains (or, "nests") the constants into a single
